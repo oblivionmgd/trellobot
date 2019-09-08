6df134c87c3638a977bac1148dadb0226fc52ef4
OblivionMGD <ephixa6022@gmail.com>
2019-09-08 22:51:08 +0900

    todo for loop

--- trellobot.py
+++ trellobot.py
@@ -19,7 +19,8 @@ TOKEN = 'NjE2MTQwMzAwMDU3OTY4NjUx.XWZufQ.rVrST3vHScWYjmokVDkWn050kpE'
 board = tr_client.list_boards()[7]
 todo = board.list_lists()[0]
 todo_option = board.list_lists()[1]
-card = todo.list_cards()
+for card in todo:
+    todo_cards += card + '\n'
 
 #discord help
 command_help = '/todo:Todoリスト内のカードを表示します。\n/shinchoku:進捗を聞かれます。'
@@ -38,7 +39,7 @@ async def on_message(message):
         await message.channel.send(command_help)
 
     if message.content == '/todo':
-        await message.channel.send(card.name)
+        await message.channel.send(todo_cards)
 
     if message.content == '/shinchoku':
         await message.channel.send('進捗どうですか')
